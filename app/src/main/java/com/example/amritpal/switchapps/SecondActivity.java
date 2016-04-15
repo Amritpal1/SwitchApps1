22d5c345e37e785bbd74246d060e8114dd4928f4
Amritpal1 <asingh59@nyit.edu>
2016-04-15 01:11:15 -0400

    Changed Play Activity to be sensor landscape according to user.

--- app/src/main/java/com/example/amritpal/switchapps/SecondActivity.java
+++ app/src/main/java/com/example/amritpal/switchapps/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.amritpal.switchapps;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +12,7 @@ import android.widget.Button;
 /**
  * Created by Amritpal on 4/11/2016.
  */
-public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
+public class SecondActivity extends AppCompatActivity {
 
     Button btn2;
 
@@ -20,27 +21,30 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        btn2 = (Button)findViewById(R.id.button2);
-        btn2.setOnClickListener(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        btn2 = (Button)findViewById(R.id.button2);
+//        btn2.setOnClickListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "You will be able to play Solitaire here", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "You will be able to study Solitaire rules here", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 //the fab button return to main menu
-                Intent myintent = new Intent(SecondActivity.this, MainActivity.class);
-                startActivity(myintent);
+//                Intent myintent = new Intent(SecondActivity.this, MainActivity.class);
+//                startActivity(myintent);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void onClick(View v){
-        Intent myintent = new Intent(this, MainActivity.class);
-        startActivity(myintent);
-    }
+//    public void onClick(View v){
+//        Intent myintent = new Intent(this, MainActivity.class);
+//        startActivity(myintent);
+//    }
     public void onHomeClick(View v) {
         Intent myintent = new Intent(this, MainActivity.class);
         startActivity(myintent);
